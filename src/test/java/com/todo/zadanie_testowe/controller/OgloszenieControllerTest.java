c8a279702cf7f5eb9792af9d36f5ad926b1980ef
kapete68 <raybanmati@gmail.com>
2025-09-01 08:24:07 +0200

    Poprawa konfiguracji testu dla OgloszenieControllerTest - nie potrzebne nawisay przy SpringBootTest

--- src/test/java/com/todo/zadanie_testowe/controller/OgloszenieControllerTest.java
+++ src/test/java/com/todo/zadanie_testowe/controller/OgloszenieControllerTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest()
+@SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 class OgloszenieControllerTest {
